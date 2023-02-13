b0db350a7cdd1cf4eaaf6a8083d12865cb6fd26a
YutaUra <yuuta3594@outlook.jp>
2023-02-13 14:42:57 +0900

    image format

--- app/modules/common/Hero/Hero.tsx
+++ app/modules/common/Hero/Hero.tsx
@@ -14,18 +14,19 @@ export const Hero = () => {
           responsive={[
             {
               size: {
-                width: 480,
-                height: 480 * MOBILE_HERO_RATIO,
+                width: 280,
+                height: 280 * MOBILE_HERO_RATIO,
               },
             },
           ]}
           options={{
             position: "left",
             contentType: MimeType.WEBP,
+            blurRadius: 1,
           }}
         />
 
-        <h1 className="absolute bottom-5 max-w-[90%] bg-gradient-to-t from-gray-600/90 to-transparent break-keep left-4 mix-blend-difference text-[rgba(220,220,220,0.9)] text-4xl font-hero">
+        <h1 className="absolute bottom-5 max-w-[90%] break-keep left-4 mix-blend-difference text-[rgba(220,220,220,0.9)] text-4xl font-hero">
           横浜市立大学
           <wbr />
           柔道部
@@ -40,18 +41,19 @@ export const Hero = () => {
           responsive={[
             {
               size: {
-                width: 960,
-                height: 960 * TABLET_HERO_RATIO,
+                width: 720,
+                height: 720 * TABLET_HERO_RATIO,
               },
             },
           ]}
           options={{
             position: "left",
             contentType: MimeType.WEBP,
+            blurRadius: 1,
           }}
         />
 
-        <h1 className="absolute bottom-5 bg-gradient-to-t from-gray-600/90 to-transparent break-keep left-8 mix-blend-difference text-[rgba(220,220,220,0.9)] text-6xl font-hero">
+        <h1 className="absolute bottom-5 break-keep left-8 mix-blend-difference text-[rgba(220,220,220,0.9)] text-6xl font-hero">
           横浜市立大学
           <wbr />
           柔道部
